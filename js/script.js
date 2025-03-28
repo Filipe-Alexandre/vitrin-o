70aed7e88082cb58b177bbbbce56f6a623ec5ddb
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2025-03-28 20:16:43 -0300

    Adicionando projeto
    passando todos arquivos

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const btnSobre = document.querySelector(".sobre")
+const btnMissao = document.querySelector(".missao")
+const btnVisao = document.querySelector(".visao")
+const btnValores = document.querySelector(".valores")
+
+const sobre = document.querySelector(".sobreDiv");
+const missao = document.querySelector(".missaoDiv");
+const visao = document.querySelector(".visaoDiv");
+const valores = document.querySelector(".valoresDiv");
+
+
+btnSobre.addEventListener("click", ()=> {
+    sobre.style.display = "flex";
+    missao.style.display = "none";
+    visao.style.display = "none";
+    valores.style.display = "none";
+})
+
+btnMissao.addEventListener("click", ()=> {
+    sobre.style.display = "none";
+    missao.style.display = "flex";
+    visao.style.display = "none";
+    valores.style.display = "none";
+})
+
+btnVisao.addEventListener("click", ()=> {
+    sobre.style.display = "none";
+    missao.style.display = "none";
+    visao.style.display = "flex";
+    valores.style.display = "none";
+})
+
+btnValores.addEventListener("click", ()=> {
+    sobre.style.display = "none";
+    missao.style.display = "none";
+    visao.style.display = "none";
+    valores.style.display = "flex";
+})
+
+
+const darkBtn = document.querySelector(".darkBtn")
+const lightBtn = document.querySelector(".lightBtn")
+const body = document.body
+
+darkBtn.addEventListener("click", ()=> {
+    body.classList.remove("lightMode");
+    body.classList.add("darkMode");
+
+}) 
+
+
+lightBtn.addEventListener("click", ()=> {
+    body.classList.remove("darkMode");
+    body.classList.add("lightMode");
+
+})
